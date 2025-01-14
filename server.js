bcaeec8607fa1fcbf267111dd4e4b37cac5080f2
damunos <damunos@gmail.com>
2025-01-14 00:39:00 -0500

    servejs postgre

--- server.js
+++ server.js
@@ -12,15 +12,12 @@ app.use(express.json());
 
 // PostgreSQL connection
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false, // Required for Railway
+    rejectUnauthorized: false,
   },
 });
+
 
 // Healthcheck route
 app.get('/health', (req, res) => {
