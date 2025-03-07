7dc26f26b4cd158d40208c940a4f92945d716371
damunos <damunos@gmail.com>
2025-03-07 18:10:43 -0500

    indjs

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './index.css';
 
 ReactDOM.render(
   <BrowserRouter basename="/Ink-N-Thread">
     <App />
   </BrowserRouter>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
