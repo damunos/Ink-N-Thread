03e3646373d420d95bafa8b5461871deab39a746
damunos <damunos@gmail.com>
2025-03-08 18:06:53 -0500

    feat: add initial Products page and include robots.txt and favicon files

---
+++ src/pages/Products.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Products = () => {
+  return (
+    <div>
+      <h1>Our Products</h1>
+      <p>Explore our screen printing and embroidery options.</p>
+    </div>
+  );
+};
+
+export default Products;
